172df0610104241eeaf43e0a99b7f5510a63a593
neumanmt <michaelneuman2020@u.northwestern.edu>
2017-10-04 15:45:11 -0500

    added files from laptop

---
+++ src/java/simpledb/RecordId.java
@@ -0,0 +1,73 @@
+package simpledb;
+
+import java.io.Serializable;
+
+/**
+ * A RecordId is a reference to a specific tuple on a specific page of a
+ * specific table.
+ */
+public class RecordId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private PageId page_id;
+    private int tuple_number;
+    /**
+     * Creates a new RecordId referring to the specified PageId and tuple
+     * number.
+     * 
+     * @param pid
+     *            the pageid of the page on which the tuple resides
+     * @param tupleno
+     *            the tuple number within the page.
+     */
+    public RecordId(PageId pid, int tupleno) {
+        this.page_id = pid;
+        this.tuple_number = tupleno;
+    }
+
+    /**
+     * @return the tuple number this RecordId references.
+     */
+    public int getTupleNumber() {
+        return tuple_number;
+    }
+
+    /**
+     * @return the page id this RecordId references.
+     */
+    public PageId getPageId() {
+        return page_id;
+    }
+
+    /**
+     * Two RecordId objects are considered equal if they represent the same
+     * tuple.
+     * 
+     * @return True if this and o represent the same tuple
+     */
+    @Override
+    public boolean equals(Object o) {
+    	if (o == null || o.getClass() != this.getClass()) {
+    		return false;
+    	}
+    	RecordId obj = ((RecordId) o);
+    	if (!obj.getPageId().equals(this.getPageId()) || obj.getTupleNumber() != this.getTupleNumber()) {
+    		return false;
+    	}
+    	return true;
+	}
+
+    /**
+     * You should implement the hashCode() so that two equal RecordId instances
+     * (with respect to equals()) have the same hashCode().
+     * 
+     * @return An int that is the same for equal RecordId objects.
+     */
+    @Override
+    public int hashCode() {
+    	String h = "" + page_id.hashCode() + tuple_number;
+    	return h.hashCode();
+    }
+
+}
